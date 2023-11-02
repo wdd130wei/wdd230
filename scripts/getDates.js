d150469cea25bc4b120f9586ac333e9a5239e0e5
wdd130wei <zhiwade.wei@gmail.com>
2023-11-03 11:29:07 +1300

    first update

---
+++ scripts/getDates.js
@@ -0,0 +1,6 @@
+const todaysdate = new Date();
+const year = todaysdate.getFullYear();
+document.getElementById('currentyear').textContent = year;
+
+let oLastModified = new Date(document.lastModified);
+document.getElementById('lastModified').textContent = 'Last modification: ' + oLastModified;
