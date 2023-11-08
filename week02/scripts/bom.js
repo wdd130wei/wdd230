12cee889ed186633f73aa287600fa9e61f7f7e75
wdd130wei <zhiwade.wei@gmail.com>
2023-11-08 20:21:05 +1300

    update

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -3,23 +3,24 @@ const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
 button.addEventListener('click', function() {
-    const li = document.createElement('li');
-    const deleteButton = document.createElement('button');
-
-    li.textContent = input.value;
-    deleteButton.textContent = 'X';
-
-    li.append(deleteButton);
-    list.append(li);
-
-    deleteButton.addEventListener('click', function() {
-        list.removeChild(li)
+    if (input.value != '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        
+        li.textContent = input.value;
+        deleteButton.textContent = 'X';
+    
+        li.append(deleteButton);
+        list.append(li);
+    
+        deleteButton.addEventListener('click', function() {
+            list.removeChild(li)
+            input.focus();
+        });
+    
         input.focus();
-    });
-
-    input.focus();
-    input.value = '';
-
+        input.value = '';
+    } 
 });
 
 
