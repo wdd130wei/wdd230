22d433a54a660f95b2f559d3c7382b9c00c2860d
wdd130wei <zhiwade.wei@gmail.com>
2023-11-08 17:15:43 +1300

    update

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -4,3 +4,34 @@ document.getElementById('currentyear').textContent = year;
 
 let oLastModified = new Date(document.lastModified);
 document.getElementById('lastModified').textContent = 'Last modification: ' + oLastModified;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+const modeButton = document.querySelector('#mode');
+const main = document.querySelector('main')
+const links = document.querySelectorAll('.card a');
+
+modeButton.addEventListener('click', () => {
+    if (modeButton.textContent.includes("🕶️")) {
+        links.forEach(link => {
+            link.style.color = '#fff';
+        });
+        main.style.background = '#000';
+        main.style.color = '#fff';
+        modeButton.textContent = "🔆";
+    }
+    else {
+        links.forEach(link => {
+            link.style.color = '#000';
+        });
+		main.style.background = "#FFFFFF";
+		main.style.color = "#000";
+        modeButton.textContent = "🕶️";
+    }
+})
