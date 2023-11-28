38c607496a1656174b687b479d8a946289cc794d
wdd130wei <zhiwade.wei@gmail.com>
2023-11-28 16:16:33 +1300

    update

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -1,37 +1,39 @@
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
 
 const cards = document.querySelector('#cards');
+const header = document.querySelector('header');
 
 async function getProphetData(url) {
   const response = await fetch(url);
   const data = await response.json();
-  //console.table(data.prophets);
-  displayProphets(data.prophets); // note that we reference the prophets array of the JSON data object, not just the object
+
+  displayProphets(data.prophets);
 }
 
-getProphetData();
-
+getProphetData(url);
 
 const displayProphets = (prophets) => {
   prophets.forEach((prophet) => {
-    // Create elements to add to the div.cards element
     let card = document.createElement('section');
-    let fullName = document.createElement('__'); // fill in the blank
+    let fullName = document.createElement('h2');
+    let birthDate = document.createElement('p');
+    let birthPlace = document.createElement('p');
     let portrait = document.createElement('img');
 
-    // Build the h2 content out to show the prophet's full name
-    fullName.textContent = `${prophet._____} ____________`; // fill in the blank
-    // Build the image portrait by setting all the relevant attributes
+    fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+    birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+    birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
     portrait.setAttribute('src', prophet.imageurl);
-    portrait.setAttribute('alt', `Portrait of ${prophet.____} ______________`); // fill in the blank
+    portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
     portrait.setAttribute('loading', 'lazy');
     portrait.setAttribute('width', '340');
     portrait.setAttribute('height', '440');
 
-    // Append the section(card) with the created elements
-    card.appendChild(_______); //fill in the blank
+    card.appendChild(fullName);
+    card.appendChild(birthDate);
+    card.appendChild(birthPlace);
     card.appendChild(portrait);
 
     cards.appendChild(card);
-  }); // end of arrow function and forEach loop
-}
+  });
+};
