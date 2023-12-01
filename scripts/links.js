2a810110d3e6e80cb22e97b6cc1f9f587bd8ceb9
wdd130wei <zhiwade.wei@gmail.com>
2023-12-01 23:50:56 +1300

    update

--- scripts/links.js
+++ scripts/links.js
@@ -1,1 +1,32 @@
-const baseURL = "https://yourgithubusername.github.io/wdd230/";
+const baseURL = "https://github.com/wdd130wei/wdd230/";
+
+const linksURL = "https://github.com/wdd130wei/wdd230/blob/main/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data);
+  }
+  
+getLinks();
+
+
+const displayLinks = (links) => {
+    links.forEach((link) => {
+      let week = document.createElement('p');
+      let url = document.createElement('a');
+      let title = document.createElement('li');
+
+      week.textContent = `${link.week}`;
+      url.textContent = `${link.links.url}`;
+      title.textContent = `${link.links.title}`;
+
+      card.appendChild(week);
+      card.appendChild(url);
+      card.appendChild(title);
+  
+      cards.appendChild(card1);
+    });
+  };
+
+
