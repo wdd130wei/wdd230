6c89902a4e9cb6cb152f3f5dd365b0d453c21b2d
wdd130wei <zhiwade.wei@gmail.com>
2023-12-11 21:06:53 +1300

    update

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -124,3 +124,39 @@ listbutton.addEventListener("click", () => {
 	display.classList.add("list");
 	display.classList.remove("grid");
 });
+
+
+/* Weather */
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#weather-desc');
+
+const apiKey = 'dce54304461cc7d7e0f2bce3ecdbc1a0';
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=26.20&lon=119.54&appid=${apiKey}&units=imperial`;
+
+async function apiFetch(url) {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data)
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src',iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
+
+apiFetch(url);
